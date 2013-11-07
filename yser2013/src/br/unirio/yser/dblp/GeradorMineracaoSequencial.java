4472e02306853fc8cca5fda65dfac8b10b591953
Marcio Barros <marcio.barros@gmail.com>
2013-11-07 17:19:30 -0200

    Mudanças na geração do arquivo de mineração sequencial

--- yser2013/src/br/unirio/yser/dblp/GeradorMineracaoSequencial.java
+++ yser2013/src/br/unirio/yser/dblp/GeradorMineracaoSequencial.java
@@ -1,5 +1,8 @@
 package br.unirio.yser.dblp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import br.unirio.yser.model.Pesquisador;
@@ -11,13 +14,42 @@ public class GeradorMineracaoSequencial
 	{
 		for (Pesquisador pesquisador : pesquisadores)
 		{
+			ArrayList<Publicacao> publicacoes = new ArrayList<Publicacao>();
+
 			for (Publicacao publicacao : pesquisador.pegaPublicacoes())
-			{
-				String nome = publicacao.getVeiculo().replace(" ", "_").replace(".", "");
-				System.out.print(nome + "\t");
-			}
+				publicacoes.add(publicacao);
 			
-			System.out.println();
+			Collections.sort(publicacoes, new ComparadorPublicacoes());
+			
+			if (publicacoes.size() > 0)
+			{
+				int ultimoAno = publicacoes.get(publicacoes.size()-1).getAno();
+				
+				for (int i = publicacoes.size()-1; i >= 0; i--)
+				{
+					Publicacao publicacao = publicacoes.get(i);
+					
+					if (publicacao.getAno() < ultimoAno)
+					{
+						System.out.print("-1\t");
+						ultimoAno = publicacao.getAno();
+					}
+						
+					String nome = publicacao.getVeiculo().replace(" ", "_").replace(".", "").replace(",", "").replace("(", "").replace(")", "");
+					System.out.print(nome + "\t");
+				}
+			
+				System.out.println("-2");
+			}
 		}
 	}
+}
+
+class ComparadorPublicacoes implements Comparator<Publicacao>
+{
+	@Override
+	public int compare(Publicacao p1, Publicacao p2)
+	{
+		return p1.getAno() - p2.getAno();
+	}	
 }
